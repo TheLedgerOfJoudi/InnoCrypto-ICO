041eb81039b1e175a75bf6486ff4b2fe06912080
hadi <hadi.mohammad.saleh1@gmail.com>
2021-09-30 16:37:30 +0300

    issues with transfer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Web3 from 'web3';
 import './App.css';
 import BalanceOf from './components/BalanceOf';
 import Header from './components/Header';
+import Transfer from './components/Transfer';
 function App() {
   return (
     <div className="App">
      <Header/>
      <BalanceOf/>
+     <Transfer/>
     </div>
   );
 }
---
+++ src/components/Transfer.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Web3 from "web3";
+import { ABI, TOKEN_ADDRESS } from "../config";
+
+class Transfer extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            receiver:"",
+            numOfTokens:""
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event){
+        const {name , value} = event.target
+        this.setState({
+            [name] : value
+        })
+       
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+        const web3 = new Web3(Web3.givenProvider)
+        web3.eth.getAccounts().then(accounts =>{
+            console.log(accounts[0])
+            const Contract = new web3.eth.Contract(ABI, TOKEN_ADDRESS)
+            console.log(this.state)
+            Contract.methods.transfer(this.state.receiver, this.state.numOfTokens)
+            .call({from : accounts[0]}).then((res) => {console.log(res)})
+        })
+
+    }
+
+    render(){
+        return(
+        <form onSubmit = {this.handleSubmit}>
+            <input type = "text" 
+            name = "receiver"
+            placeholder = "To whom you want to send" 
+            value = {this.state.receiver} 
+            onChange = {this.handleChange}
+            />
+            
+            <input type = "text"
+            name = "numOfTokens" 
+            placeholder = "How much are you willing to send" 
+            value = {this.state.numOfTokens}
+            onChange = {this.handleChange}
+            />
+            <br/>
+            <button type = "submit">Transfer tokens</button>
+        </form>
+        )
+    }
+}
+export default Transfer
