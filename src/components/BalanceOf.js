cd22846ba9f0370abc76fd41387568a8a6caaa15
hadi <hadi.mohammad.saleh1@gmail.com>
2021-09-30 13:18:58 +0300

    initial commit

---
+++ src/components/BalanceOf.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Web3 from "web3";
+import { ABI, TOKEN_ADDRESS } from "../config";
+class BalanceOf extends React.Component{
+constructor(){
+    super()
+    this.state = {
+        address:"",
+        numOfTokens:""
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+}
+
+handleChange(event){
+this.setState({
+    address : event.target.value
+})
+}
+
+handleSubmit(event){
+    event.preventDefault()
+    const web3 = new Web3(Web3.givenProvider)
+    web3.eth.getAccounts().then((accounts => {
+        const Contract = new web3.eth.Contract(ABI, TOKEN_ADDRESS)
+        Contract.methods.balanceOf(this.state.address).call({from : accounts[0]})
+        .then(res => {
+            this.setState({
+                numOfTokens : res
+            })
+        })
+    }))
+}
+
+render(){
+    return(
+        <div>
+        <form onSubmit = {this.handleSubmit}>
+            <input type = "text" value = {this.state.address} onChange = {this.handleChange}/>
+            <button type = "submit">Get Balance</button>
+        </form>
+        <h3>
+            this address has {this.state.numOfTokens} InnoCryptos
+        </h3>
+        </div>
+    )
+}
+}
+export default BalanceOf;
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Web3 from "web3";
+class Header extends React.Component{
+constructor(){
+    super()
+    this.state = {
+        network : ""
+    }
+}
+
+    componentDidMount(){
+        this.loadNet();
+    }
+
+    async loadNet(){
+        const web3 = new Web3(Web3.givenProvider)
+        await web3.eth.net.getNetworkType().then((result) =>{
+            this.setState({
+                network : result
+            })
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Welcome to InnoCrypto ICO!</h1>
+                <h2>You are on network : {this.state.network}</h2>
+            </div>
+        )
+    }
+}
+
+export default Header;
